16de4718fc5c00f4c718f40b45ff8da8753b678b
Adarsh <2017csb1064@gmail.com>
2018-09-10 19:00:34 +0530

    music fixed

--- all_events/music.php
+++ all_events/music.php
@@ -92,7 +92,7 @@
     <div class="overlay__item" id="preview-1">
         <div class="box">
             <div class="box__shadow"></div>
-            <img class="box__img box__img--original" src="img/original/Solo Singing(Indian).jpg" alt="Some image" />
+            <img class="box__img box__img--original" src="img/original/saaz.jpg" alt="Some image" />
             <h3 class="box__title"><span class="box__title-inner">0K</span></h3>
             <h4 class="box__text"><span class="box__text-inner">SAAZ(Solo Indian)</span></h4>
             <div class="box__deco">&#10014;</div>
@@ -107,7 +107,7 @@
     <div class="overlay__item" id="preview-2">
         <div class="box">
             <div class="box__shadow"></div>
-            <img class="box__img box__img--original" src="img/original/Solo Singing(Western).jpg" alt="Some image" />
+            <img class="box__img box__img--original" src="img/original/adagio.jpg" alt="Some image" />
             <h3 class="box__title"><span class="box__title-inner">0K</span></h3>
             <h4 class="box__text box__text--bottom box__text--right"><span class="box__text-inner box__text-inner--rotated3">ADAGIO(SOLO Western)</span></h4>
         </div>
